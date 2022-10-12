5914ad63d72b9b644dae7c88e03f189abade41ce
Fernando Stordeur <fstordeur@gmail.com>
2022-10-12 16:22:26 -0300

    cambio nombres

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
 
 function validar(){
-    var todo_correcto = true;
+    var correcto = true;
       
     if(document.getElementById("firstname").value.length < 2 ){
-        todo_correcto = false;
+        correcto = false;
     }
 
     if(document.getElementById("lasttname").value.length < 2 ){
-        todo_correcto = false;
+        correcto = false;
     }
     
     var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
     var mail = document.form1.mail.value;
     if (!expresion.test(mail)){
-        todo_correcto = false;
+        correcto = false;
     }
 
-    if(!todo_correcto){
-    alert('Algunos campos no están correctos, vuelva a revisarlos');
+    if(!correcto){
+    alert("los capmpos no estan completos, reviselos");
     }
     
-    return todo_correcto;
+    return correcto;
     }
     
